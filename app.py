e60e38395243f30a44110c6bc12e4d4f844fef72
William Lees <william@lees.org.uk>
2019-08-19 18:24:33 +0100

    Don't nest genotype metadata fetch

--- app.py
+++ app.py
@@ -1927,44 +1927,45 @@ def tidy_genotype():
                     except:
                         report += 'Cant fetch run id details for accession ' + run_id + '<br>'
 
-                for gen in sub.genotype_descriptions:
-                    for rec in gen.sample_names:
-                        db.session.delete(rec)
+            for gen in sub.genotype_descriptions:
+                report += 'Processing genotype' + gen.genotype_name + '<br>'
+                for rec in gen.sample_names:
+                    db.session.delete(rec)
 
-                    sam_ids = gen.genotype_biosample_ids
-                    sam_ids = sam_ids.replace(',', ' ')
-                    sam_ids = sam_ids.replace(';', ' ')
-                    sam_ids = sam_ids.split()
+                sam_ids = gen.genotype_biosample_ids
+                sam_ids = sam_ids.replace(',', ' ')
+                sam_ids = sam_ids.replace(';', ' ')
+                sam_ids = sam_ids.split()
 
-                    for sam_id in sam_ids:
-                        try:
-                            resp = get_ena_samn_details(sam_id)
-                            rec = SampleName()
-                            rec.sam_accession_no = sam_id
-                            rec.sam_record_title = resp['title']
-                            rec.sam_url = resp['url']
-                            gen.sample_names.append(rec)
-                        except ValueError as e:
-                            report += 'Cant fetch sample details for accession ' + sam_id + '<br>'
+                for sam_id in sam_ids:
+                    try:
+                        resp = get_ena_samn_details(sam_id)
+                        rec = SampleName()
+                        rec.sam_accession_no = sam_id
+                        rec.sam_record_title = resp['title']
+                        rec.sam_url = resp['url']
+                        gen.sample_names.append(rec)
+                    except ValueError as e:
+                        report += 'Cant fetch sample details for accession ' + sam_id + '<br>'
 
-                    for rec in gen.record_set:
-                        db.session.delete(rec)
+                for rec in gen.record_set:
+                    db.session.delete(rec)
 
-                    run_ids = gen.genotype_run_ids
-                    run_ids = run_ids.replace(',', ' ')
-                    run_ids = run_ids.replace(';', ' ')
-                    run_ids = run_ids.split()
+                run_ids = gen.genotype_run_ids
+                run_ids = run_ids.replace(',', ' ')
+                run_ids = run_ids.replace(';', ' ')
+                run_ids = run_ids.split()
 
-                    for run_id in run_ids:
-                        try:
-                            resp = get_ena_srr_details(run_id)
-                            rec = RecordSet()
-                            rec.rec_accession_no = run_id
-                            rec.rec_record_title = resp['title']
-                            rec.rec_url = resp['url']
-                            gen.record_set.append(rec)
+                for run_id in run_ids:
+                    try:
+                        resp = get_ena_srr_details(run_id)
+                        rec = RecordSet()
+                        rec.rec_accession_no = run_id
+                        rec.rec_record_title = resp['title']
+                        rec.rec_url = resp['url']
+                        gen.record_set.append(rec)
 
-                        except ValueError as e:
-                            report += 'Cant fetch recordset details for accession ' + run_id + '<br>'
+                    except ValueError as e:
+                        report += 'Cant fetch recordset details for accession ' + run_id + '<br>'
                 db.session.commit()
     return(report)
