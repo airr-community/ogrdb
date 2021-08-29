5f3785a050e669880554805d7fa7fa39bb1fb27c
William Lees <william@lees.org.uk>
2021-08-29 16:45:49 +0100

    Rework to handle new ENA query structure

--- get_ena_details.py
+++ get_ena_details.py
@@ -23,10 +23,11 @@ def get_ena_project_details(prj_id):
         raise ValueError('bady formatted project id: %s' % (prj_id))
 
     try:
-        r = requests.get('https://www.ebi.ac.uk/ena/data/view/%s&display=xml' % prj_id)
+        r = requests.get('https://www.ebi.ac.uk/ena/browser/api/xml/%s' % prj_id)
 
         if r.status_code != 200:
             raise ValueError('Unexpected response from ENA: status %d' % r.status_code)
+        foo = r.content.decode("utf-8")
         root = ET.fromstring(r.content)
     except Exception as e:
         exc_type, exc_value = sys.exc_info()[:2]
@@ -36,7 +37,7 @@ def get_ena_project_details(prj_id):
 
     if len(titles) > 0:
         ret['title'] = titles[0].text if len(titles) > 0 else ''
-        ret['url'] = 'https://www.ebi.ac.uk/ena/data/view/%s' % prj_id
+        ret['url'] = 'https://www.ebi.ac.uk/ena/browser/api/xml/%s' % prj_id
     else:
         raise ValueError('Error fetching nucleotide info from ENA: %s' % (root.text))
 
@@ -46,24 +47,36 @@ def get_ena_project_details(prj_id):
 def get_ena_nuc_details(nuc_id):
     ret = {}
 
-    if len(nuc_id) < 5 or (nuc_id[:2] != 'MK' and nuc_id[:2] != 'MN'):
+    if len(nuc_id) < 5 or (nuc_id[:2] != 'MK' and nuc_id[:2] != 'MN' and nuc_id[:2] != 'OU'):
         raise ValueError('badly formatted nucleotide sequence record accession number: %s' % (nuc_id))
 
     try:
-        r = requests.get('https://www.ebi.ac.uk/ena/data/view/%s&display=xml' % nuc_id)
-        if r.status_code != 200:
-            raise ValueError('Unexpected response from ENA: status %d' % r.status_code)
-
-        root = ET.fromstring(r.content)
-        entries = root.findall("./entry/description")
+        title = ''
+        request = 'https://www.ebi.ac.uk/ena/browser/api/xml/%s' % nuc_id
+        r = requests.get(request)
+        if r.status_code == 200:
+            root = ET.fromstring(r.content)
+            entries = root.findall("./entry/description")
+            title = entries[0].text
+        else:
+            request = 'https://www.ebi.ac.uk/ena/browser/api/embl/%s' % nuc_id
+            r = requests.get(request)
+            if r.status_code == 200:
+                lines = r.content.decode('utf-8')
+                lines = lines.split('\n')
+                for line in lines:
+                    if line[:4] == 'DE  ':
+                        title += line[4:]
+            else:
+                raise ValueError('Unexpected response from ENA: status %d' % r.status_code)
 
     except Exception as e:
         exc_type, exc_value = sys.exc_info()[:2]
         raise ValueError('Error fetching nucleotide info from ENA: %s' % (exc_value))
 
-    if len(entries) > 0:
-        ret['title'] = entries[0].text
-        ret['url'] = 'https://www.ebi.ac.uk/ena/data/view/%s' % nuc_id
+    if len(title) > 0:
+        ret['title'] = title
+        ret['url'] = request
     else:
         raise ValueError('Error fetching nucleotide info from ENA: %s' % (root.text))
 
@@ -73,11 +86,11 @@ def get_ena_nuc_details(nuc_id):
 def get_ena_srr_details(srr_id):
     ret = {}
 
-    if len(srr_id) < 5 or srr_id[:3] not in ('ERX', 'SRR'):
+    if len(srr_id) < 5 or srr_id[:3] not in ('ERX', 'SRR', 'ERR'):
         raise ValueError('badly formatted sequence set accession number: %s' % (srr_id))
 
     try:
-        r = requests.get('https://www.ebi.ac.uk/ena/data/view/%s&display=xml' % srr_id)
+        r = requests.get('https://www.ebi.ac.uk/ena/browser/api/xml/%s' % srr_id)
         if r.status_code != 200:
             raise ValueError('Unexpected response from ENA: status %d' % r.status_code)
 
@@ -87,9 +100,9 @@ def get_ena_srr_details(srr_id):
         exc_type, exc_value = sys.exc_info()[:2]
         raise ValueError('Error fetching nucleotide info from ENA: %s' % (exc_value))
 
-    if srr_id[:3] == 'ERX':
-        titles = root.findall("./EXPERIMENT/TITLE")
-    else:
+    titles = root.findall("./EXPERIMENT/TITLE")
+
+    if len(titles) == 0:
         titles = root.findall("./RUN/TITLE")
 
     if len(titles) > 0:
@@ -107,7 +120,7 @@ def get_ena_samn_details(sam_id):
         raise ValueError('badly formatted sample accession number: %s' % (sam_id))
 
     try:
-        r = requests.get('https://www.ebi.ac.uk/ena/data/view/%s&display=xml' % sam_id)
+        r = requests.get('https://www.ebi.ac.uk/ena/browser/api/xml/%s' % sam_id)
         if r.status_code != 200:
             raise ValueError('Unexpected response from ENA: status %d' % r.status_code)
 
