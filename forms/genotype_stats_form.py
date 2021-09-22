e241d2933def7ddedf41e53329f8cd8edb40eb4c
William Lees <william@lees.org.uk>
2021-09-22 16:11:34 +0100

    Update wording and threhold default in statistics report

--- forms/genotype_stats_form.py
+++ forms/genotype_stats_form.py
@@ -22,15 +22,15 @@ class GeneStatsForm(FlaskForm):
     rare_threshold = DecimalField('Low-Frequency Threshold', [NumberRange(min=0.0, max=100.0)], description="Threshold to apply to identified low-frequency genes", places=2, default=0.05)
     very_rare_genes = StringField('Very Low-Frequency Genes', description="Comma-separated list of genes known to be present at a very low frequency, for which an even lower threshold should be applied", default='IGHV1-45, IGHV4-28')
     very_rare_threshold = DecimalField('Very Low-Frequency Threshold', [NumberRange(min=0.0, max=100.0)], description="Threshold to apply to identified very low-frequency genes", places=2, default=0.02)
-    allelic_threshold = DecimalField('Minimum Allelic Percentage', [NumberRange(min=0.0, max=100.0)], description="Minimum % of unmutated sequences assigned to this allele as opposed to other alleles of the same gene", places=2, default=20.0)
-    assigned_unmutated_threshold = DecimalField('Minimum Unmutated Percentage', [NumberRange(min=0.0, max=100.0)], description="Minimum % of sequences assigned to this allele that are unmutated", places=2, default=1.00)
+    allelic_threshold = DecimalField('Minimum Allelic Percentage', [NumberRange(min=0.0, max=100.0)], description="The number of sequences exactly matching the sequence of this allele divided by the number of sequences exactly matching any allele of this specific gene must meet this threshold", places=2, default=20.0)
+    assigned_unmutated_threshold = DecimalField('Minimum Unmutated Percentage', [NumberRange(min=0.0, max=100.0)], description="The number of sequences exactly matching this allele divided by the number of sequences assigned to this allele must meet this threshold", places=2, default=20.00)
     create = SubmitField('View Report')
 
 
 class GeneStats_table(StyledTable):
     gene = StyledCol("Allele")
     occurrences = StyledCol("Occurrences", tooltip="Number of genotypes including the allele")
-    unmutated_freq = StyledCol("Unmutated Frequency", tooltip="Unmutated frequency, averaged over all included genotypes")
+    unmutated_freq = StyledCol("Unmutated Frequency", tooltip="The number of sequences exactly matching the sequence of this allele divided by the number of sequences exactly matching any allele of any gene, expressed as a percentage, averaged over all included genotypes")
 
 
 def make_GeneStats_table_table(results, classes=()):
