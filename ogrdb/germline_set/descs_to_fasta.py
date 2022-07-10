be0de168554bedaa5cfe26115f11cce069fca2f7
William Lees <william@lees.org.uk>
2022-07-10 08:46:27 +0100

    Fix fasta format. Add help text for poster and receptor-germline-tools.

--- ogrdb/germline_set/descs_to_fasta.py
+++ ogrdb/germline_set/descs_to_fasta.py
@@ -5,8 +5,6 @@ def descs_to_fasta(descs, format):
     ret = ''
     for desc in descs:
         name = desc.sequence_name
-        if desc.imgt_name != '':
-            name += '|IMGT=' + desc.imgt_name
         if format == 'gapped':
             ret += format_fasta_sequence(name, desc.coding_seq_imgt, 60)
         else:
