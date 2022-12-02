59e8a7b4f92012f3a800d751c629e847b89ab75f
William Lees <william@lees.org.uk>
2022-12-02 11:11:18 +0000

    Where necessary, add fake allele and family to allele names in FASTA germline set files

--- ogrdb/germline_set/descs_to_fasta.py
+++ ogrdb/germline_set/descs_to_fasta.py
@@ -1,10 +1,17 @@
 from sequence_format import format_fasta_sequence
 
 
-def descs_to_fasta(descs, format):
+def descs_to_fasta(descs, format, fake_allele=False):
     ret = ''
     for desc in descs:
         name = desc.sequence_name
+
+        if fake_allele:
+            if '*' not in name:
+                name += '*00'
+            if name[4] == '-':
+                name = name.replace('-', '0-')
+
         if format == 'gapped':
             ret += format_fasta_sequence(name, desc.coding_seq_imgt, 60)
         else:
